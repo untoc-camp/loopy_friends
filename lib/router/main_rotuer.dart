95beb5318af12285b5c92fc2aced6e75880afda2
RDavid32 <114576793+RDavid32@users.noreply.github.com>
2024-04-09 16:04:05 +0900

    fixed untracked files

---
+++ lib/router/main_rotuer.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class MainRouter {
+  static final List<GetPage> routes = [
+    GetPage(
+      name: '/',
+      page: () => const Scaffold(
+        body: Center(
+          child: Text('Main Page'),
+        ),
+      ),
+    ),
+    GetPage(
+      name: '/SignIn',
+      page: () => const Scaffold(
+        body: Center(
+          child: Text('Second Page'),
+        ),
+      ),
+    ),
+    GetPage(
+      name: '/SignUp',
+      page: () => const Scaffold(
+        body: Center(
+          child: Text('Third Page'),
+        ),
+      ),
+    ),
+  ];
+}
