d5f5944aff1995b289f6b3f4c3ebb145b26eb95e
RDavid32 <114576793+RDavid32@users.noreply.github.com>
2024-04-12 21:24:53 +0900

    레이아웃 작성
    레이아웃 작성

---
+++ lib/view/main_page_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class MainPageVIew extends StatelessWidget {
+  const MainPageVIew({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Main Page'),
+    );
+  }
+}
