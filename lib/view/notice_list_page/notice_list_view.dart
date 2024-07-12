6dd763d41fb2b0e019da63f744a2818149f6b612
hyojeong130 <127065989+hyojeong130@users.noreply.github.com>
2024-07-12 15:12:49 +0900

    새로고침2

--- lib/view/notice_list_page/notice_list_view.dart
+++ lib/view/notice_list_page/notice_list_view.dart
@@ -10,6 +10,10 @@ class NoticeListView extends StatelessWidget {
 
   NoticeListView({super.key, required this.category});
 
+  Future<void> _refreshData() async {
+    noticeController.refreshData(category);
+  }
+
   @override
   Widget build(BuildContext context) {
     RxList<Notice> data;
@@ -35,44 +39,47 @@ class NoticeListView extends StatelessWidget {
         body: Padding(
           padding: const EdgeInsets.only(top: 10.0),
           child: Obx(() {
-            return ListView.builder(
-                itemCount: data.length,
-                itemBuilder: (context, index) {
-                  final reversedIndex = data.length - 1 - index;
-                  return GestureDetector(
-                      onTap: () {
-                        Get.to(() => DetailPageView(), arguments: data[reversedIndex]);
-                      },
-                      child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 7.0, vertical: 3.0),
-                          child: Container(
-                            height: 100,
-                            width: 340,
-                            decoration: BoxDecoration(
-                              color: Color.fromARGB(255, 212, 221, 232),
-                              borderRadius: BorderRadius.circular(12.0),
-                            ),
-                            child: Padding(
-                                padding: const EdgeInsets.symmetric(horizontal: 18.0, vertical: 18.0),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(data[reversedIndex].title,
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 20,
-                                          fontWeight: FontWeight.bold,
-                                        )),
-                                    SizedBox(height: 10),
-                                    Text(data[reversedIndex].created_at,
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 14,
-                                        ))
-                                  ],
-                                )),
-                          )));
-                });
+            return RefreshIndicator(
+              onRefresh: _refreshData,
+              child: ListView.builder(
+                  itemCount: data.length,
+                  itemBuilder: (context, index) {
+                    final reversedIndex = data.length - 1 - index;
+                    return GestureDetector(
+                        onTap: () {
+                          Get.to(() => DetailPageView(), arguments: data[reversedIndex]);
+                        },
+                        child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 7.0, vertical: 3.0),
+                            child: Container(
+                              height: 100,
+                              width: 340,
+                              decoration: BoxDecoration(
+                                color: Color.fromARGB(255, 212, 221, 232),
+                                borderRadius: BorderRadius.circular(12.0),
+                              ),
+                              child: Padding(
+                                  padding: const EdgeInsets.symmetric(horizontal: 18.0, vertical: 18.0),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text(data[reversedIndex].title,
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.bold,
+                                          )),
+                                      SizedBox(height: 10),
+                                      Text(data[reversedIndex].created_at,
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 14,
+                                          ))
+                                    ],
+                                  )),
+                            )));
+                  }),
+            );
           }),
         ));
   }
