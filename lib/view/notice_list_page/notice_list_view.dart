1937b47601c528035456af385e03b0ba2bc5477e
minecoby <minecoby@naver.com>
2024-07-12 15:31:32 +0900

    공지목록 크기조절

--- lib/view/notice_list_page/notice_list_view.dart
+++ lib/view/notice_list_page/notice_list_view.dart
@@ -35,52 +35,61 @@ class NoticeListView extends StatelessWidget {
     }
 
     return Scaffold(
-        backgroundColor: Color.fromARGB(255, 246, 246, 246),
-        body: Padding(
-          padding: const EdgeInsets.only(top: 10.0),
-          child: Obx(() {
-            return RefreshIndicator(
-              onRefresh: _refreshData,
-              child: ListView.builder(
-                  itemCount: data.length,
-                  itemBuilder: (context, index) {
-                    final reversedIndex = data.length - 1 - index;
-                    return GestureDetector(
-                        onTap: () {
-                          Get.to(() => DetailPageView(), arguments: data[reversedIndex]);
-                        },
-                        child: Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 7.0, vertical: 3.0),
-                            child: Container(
-                              height: 100,
-                              width: 340,
-                              decoration: BoxDecoration(
-                                color: Color.fromARGB(255, 212, 221, 232),
-                                borderRadius: BorderRadius.circular(12.0),
+      backgroundColor: Color.fromARGB(255, 246, 246, 246),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 10.0),
+        child: Obx(() {
+          return RefreshIndicator(
+            onRefresh: _refreshData,
+            child: ListView.builder(
+              itemCount: data.length,
+              itemBuilder: (context, index) {
+                final reversedIndex = data.length - 1 - index;
+                return GestureDetector(
+                  onTap: () {
+                    Get.to(() => DetailPageView(), arguments: data[reversedIndex]);
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 7.0, vertical: 3.0),
+                    child: Container(
+                      width: double.infinity,
+                      constraints: BoxConstraints(minHeight: 100),
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 212, 221, 232),
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 18.0, vertical: 18.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              data[reversedIndex].title,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
                               ),
-                              child: Padding(
-                                  padding: const EdgeInsets.symmetric(horizontal: 18.0, vertical: 18.0),
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Text(data[reversedIndex].title,
-                                          style: TextStyle(
-                                            color: Colors.white,
-                                            fontSize: 20,
-                                            fontWeight: FontWeight.bold,
-                                          )),
-                                      SizedBox(height: 10),
-                                      Text(data[reversedIndex].created_at,
-                                          style: TextStyle(
-                                            color: Colors.white,
-                                            fontSize: 14,
-                                          ))
-                                    ],
-                                  )),
-                            )));
-                  }),
-            );
-          }),
-        ));
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              data[reversedIndex].created_at,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 14,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          );
+        }),
+      ),
+    );
   }
 }
