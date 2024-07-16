ab6963c71f44343cd822fb9f5d5d68ec07e7fe04
DaeWook Kim <nozerose@naver.com>
2024-07-16 15:15:57 +0900

    회원가입 예외처리 구현 및 비밀번호 입력칸 보이지 않게 처리

--- lib/controller/signup_page_controller.dart
+++ lib/controller/signup_page_controller.dart
@@ -12,13 +12,65 @@ class SignUpController extends GetxController {
   final nicknameController = TextEditingController();
   final gradeController = TextEditingController();
 
+  final RegExp idPwdRegExp = RegExp(r'^[a-zA-Z0-9]+$');
+  final RegExp generalRegExp = RegExp(r'^[a-zA-Z0-9ㄱ-ㅎ가-힣]+$');
+
   void signUpButton() async {
     String apiUrl = '${Urls.apiUrl}users/';
     try {
+      // 모든 필드가 비어있는 경우 처리
+      if (idController.text.isEmpty ||
+          pwdController.text.isEmpty ||
+          pwdCheckController.text.isEmpty ||
+          nameController.text.isEmpty ||
+          nicknameController.text.isEmpty ||
+          gradeController.text.isEmpty) {
+        Get.snackbar(
+          'Error',
+          '모든 칸을 채워주세요.',
+          snackPosition: SnackPosition.BOTTOM,
+        );
+        return;
+      }
+
+      // 비밀번호 일치 여부 확인
       if (pwdController.text != pwdCheckController.text) {
         Get.snackbar(
           'Error',
           '비밀번호가 일치하지 않습니다.',
+          snackPosition: SnackPosition.BOTTOM,
+        );
+        return;
+      }
+
+      // 아이디와 비밀번호 유효성 검사
+      if (!idPwdRegExp.hasMatch(idController.text) ||
+          !idPwdRegExp.hasMatch(pwdController.text)) {
+        Get.snackbar(
+          'Error',
+          '아이디와 비밀번호는 영어와 숫자만 입력 가능합니다.',
+          snackPosition: SnackPosition.BOTTOM,
+        );
+        return;
+      }
+
+      // 일반 필드 유효성 검사 (특수문자 포함 여부)
+      if (!generalRegExp.hasMatch(nameController.text) ||
+          !generalRegExp.hasMatch(nicknameController.text)) {
+        Get.snackbar(
+          'Error',
+          '이름과 닉네임에 특수문자를 입력할 수 없습니다.',
+          snackPosition: SnackPosition.BOTTOM,
+        );
+        return;
+      }
+
+      // 학년이 숫자가 아닐 경우 처리
+      int? grade = int.tryParse(gradeController.text);
+      if (grade == null || grade < 1 || grade > 6) {
+        Get.snackbar(
+          'Error',
+          '학년에는 1에서 6 사이의 숫자만 입력해주세요.',
           snackPosition: SnackPosition.BOTTOM,
         );
         return;
@@ -32,7 +84,7 @@ class SignUpController extends GetxController {
           "password": pwdController.text,
           "realname": nameController.text,
           "nickname": nicknameController.text,
-          "grade": int.parse(gradeController.text)
+          "grade": grade
         }),
       );
 
@@ -55,7 +107,7 @@ class SignUpController extends GetxController {
     } catch (e) {
       Get.snackbar(
         'Error',
-        'Failed to connect to the server',
+        'Failed to connect to the server: $e',
         snackPosition: SnackPosition.BOTTOM,
       );
     }
--- lib/view/start_page/signup_page_view.dart
+++ lib/view/start_page/signup_page_view.dart
@@ -3,10 +3,18 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:loopy_friends/controller/signup_page_controller.dart';
 
-class SignUpPageView extends StatelessWidget {
-  SignUpPageView({super.key});
+class SignUpPageView extends StatefulWidget {
+  const SignUpPageView({super.key});
 
+  @override
+  _SignUpPageViewState createState() => _SignUpPageViewState();
+}
+
+class _SignUpPageViewState extends State<SignUpPageView> {
   final _controller = Get.put(SignUpController());
+
+  bool _isPasswordHidden = true;
+  bool _isPasswordCheckHidden = true;
 
   @override
   Widget build(BuildContext context) {
@@ -14,64 +22,94 @@ class SignUpPageView extends StatelessWidget {
       appBar: AppBar(
         title: Text('회원가입'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            TextFormField(
-              controller: _controller.idController,
-              decoration: InputDecoration(
-                labelText: '아이디',
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(), // 화면을 터치하면 키보드 숨기기
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _controller.idController,
+                decoration: InputDecoration(
+                  labelText: '아이디',
+                ),
               ),
-            ),
-            SizedBox(height: 15),
-            TextFormField(
-              controller: _controller.pwdController,
-              decoration: InputDecoration(
-                labelText: '비밀번호',
+              SizedBox(height: 15),
+              TextFormField(
+                controller: _controller.pwdController,
+                obscureText: _isPasswordHidden,
+                decoration: InputDecoration(
+                  labelText: '비밀번호',
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _isPasswordHidden
+                          ? Icons.visibility
+                          : Icons.visibility_off,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _isPasswordHidden = !_isPasswordHidden;
+                      });
+                    },
+                  ),
+                ),
               ),
-            ),
-            SizedBox(height: 15),
-            TextFormField(
-              controller: _controller.pwdCheckController,
-              decoration: InputDecoration(
-                labelText: '비밀번호확인',
+              SizedBox(height: 15),
+              TextFormField(
+                controller: _controller.pwdCheckController,
+                obscureText: _isPasswordCheckHidden,
+                decoration: InputDecoration(
+                  labelText: '비밀번호확인',
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _isPasswordCheckHidden
+                          ? Icons.visibility
+                          : Icons.visibility_off,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _isPasswordCheckHidden = !_isPasswordCheckHidden;
+                      });
+                    },
+                  ),
+                ),
               ),
-            ),
-            SizedBox(height: 15),
-            TextFormField(
-              controller: _controller.nameController,
-              decoration: InputDecoration(
-                labelText: '이름',
+              SizedBox(height: 15),
+              TextFormField(
+                controller: _controller.nameController,
+                decoration: InputDecoration(
+                  labelText: '이름',
+                ),
               ),
-            ),
-            SizedBox(height: 15),
-            TextFormField(
-              controller: _controller.nicknameController,
-              decoration: InputDecoration(
-                labelText: '닉네임',
+              SizedBox(height: 15),
+              TextFormField(
+                controller: _controller.nicknameController,
+                decoration: InputDecoration(
+                  labelText: '닉네임',
+                ),
               ),
-            ),
-            SizedBox(height: 15),
-            TextFormField(
-              controller: _controller.gradeController,
-              decoration: InputDecoration(
-                labelText: '학년',
+              SizedBox(height: 15),
+              TextFormField(
+                controller: _controller.gradeController,
+                decoration: InputDecoration(
+                  labelText: '학년',
+                ),
               ),
-            ),
-            SizedBox(height: 30),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {
-                  _controller.signUpButton();
-                },
-                child: Text('회원가입'),
-              ),
-            )
-          ],
+              SizedBox(height: 30),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    _controller.signUpButton();
+                  },
+                  child: Text('회원가입'),
+                ),
+              )
+            ],
+          ),
         ),
       ),
+      resizeToAvoidBottomInset: true, // 키보드가 올라올 때 화면을 자동으로 조정
     );
   }
 }
