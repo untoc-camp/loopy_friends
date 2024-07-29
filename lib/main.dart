c8efaafcc983df2e8d97ec8f1b50efbfbe680e6a
minecoby <minecoby@naver.com>
2024-07-29 20:15:53 +0900

    앱title수정....

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'PlacePlanPlatform',
+      title: 'LoopyFriends',
       theme: ThemeData(
         useMaterial3: true,
         fontFamily: 'Nanum Gothic',
