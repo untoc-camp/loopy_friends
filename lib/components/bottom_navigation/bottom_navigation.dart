74ce00776f4c6698809669f80b85294b87494d46
hyojeong130 <127065989+hyojeong130@users.noreply.github.com>
2024-07-31 16:06:16 +0900

    메인화면 수정

--- lib/components/bottom_navigation/bottom_navigation.dart
+++ lib/components/bottom_navigation/bottom_navigation.dart
@@ -7,66 +7,89 @@ class MyBottomNavgationBar extends GetView<MyBottomNavgationBarController> {
 
   @override
   Widget build(BuildContext context) {
-    return Obx(() => BottomNavigationBar(
-          currentIndex: controller.selectedIndex.value,
-          onTap: controller.changeIndex,
-          selectedItemColor: context.theme.colorScheme.onBackground,
-          unselectedItemColor: context.theme.colorScheme.onSurfaceVariant,
-          unselectedLabelStyle: TextStyle(fontSize: 10),
-          selectedLabelStyle: TextStyle(fontSize: 10),
-          type: BottomNavigationBarType.fixed,
-          backgroundColor: context.theme.colorScheme.background,
-          items: <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-                icon: Column(
-                  children: [
-                    Icon(
-                      Icons.home,
-                      color: controller.selectedIndex.value == 0 ? Colors.black : Colors.grey,
-                      size: 24.0,
-                      semanticLabel: 'Text to announce in accessibility modes',
+    return Obx(() => Container(
+          height: 70,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                spreadRadius: 5,
+                blurRadius: 7,
+                offset: const Offset(0, 3),
+              )
+            ],
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
+            ),
+          ),
+          child: ClipRRect(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(20),
+                topRight: Radius.circular(20),
+              ),
+              child: BottomNavigationBar(
+                currentIndex: controller.selectedIndex.value,
+                onTap: controller.changeIndex,
+                selectedItemColor: context.theme.colorScheme.onBackground,
+                unselectedItemColor: context.theme.colorScheme.onSurfaceVariant,
+                unselectedLabelStyle: TextStyle(fontSize: 10),
+                selectedLabelStyle: TextStyle(fontSize: 10),
+                type: BottomNavigationBarType.fixed,
+                backgroundColor: context.theme.colorScheme.background,
+                items: <BottomNavigationBarItem>[
+                  BottomNavigationBarItem(
+                      icon: Column(
+                        children: [
+                          Icon(
+                            Icons.home,
+                            color: controller.selectedIndex.value == 0 ? Colors.black : Colors.grey,
+                            size: 24.0,
+                            semanticLabel: 'Text to announce in accessibility modes',
+                          ),
+                        ],
+                      ),
+                      label: "홈"),
+                  BottomNavigationBarItem(
+                      icon: Column(
+                        children: [
+                          Icon(
+                            Icons.campaign,
+                            color: controller.selectedIndex.value == 1 ? Colors.black : Colors.grey,
+                            size: 24.0,
+                            semanticLabel: 'Text to announce in accessibility modes',
+                          ),
+                        ],
+                      ),
+                      label: "공지사항"),
+                  BottomNavigationBarItem(
+                      icon: Column(
+                        children: [
+                          Icon(
+                            Icons.dvr,
+                            color: controller.selectedIndex.value == 2 ? Colors.black : Colors.grey,
+                            size: 24.0,
+                            semanticLabel: 'Text to announce in accessibility modes',
+                          ),
+                        ],
+                      ),
+                      label: "커뮤니티"),
+                  BottomNavigationBarItem(
+                    icon: Column(
+                      children: [
+                        Icon(
+                          Icons.more_horiz,
+                          color: controller.selectedIndex.value == 3 ? Colors.black : Colors.grey,
+                          size: 24.0,
+                          semanticLabel: 'Text to announce in accessibility modes',
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-                label: "홈"),
-            BottomNavigationBarItem(
-                icon: Column(
-                  children: [
-                    Icon(
-                      Icons.campaign,
-                      color: controller.selectedIndex.value == 1 ? Colors.black : Colors.grey,
-                      size: 24.0,
-                      semanticLabel: 'Text to announce in accessibility modes',
-                    ),
-                  ],
-                ),
-                label: "공지사항"),
-            BottomNavigationBarItem(
-                icon: Column(
-                  children: [
-                    Icon(
-                      Icons.dvr,
-                      color: controller.selectedIndex.value == 2 ? Colors.black : Colors.grey,
-                      size: 24.0,
-                      semanticLabel: 'Text to announce in accessibility modes',
-                    ),
-                  ],
-                ),
-                label: "커뮤니티"),
-            BottomNavigationBarItem(
-              icon: Column(
-                children: [
-                  Icon(
-                    Icons.more_horiz,
-                    color: controller.selectedIndex.value == 3 ? Colors.black : Colors.grey,
-                    size: 24.0,
-                    semanticLabel: 'Text to announce in accessibility modes',
+                    label: "더보기",
                   ),
                 ],
-              ),
-              label: "더보기",
-            ),
-          ],
+              )),
         ));
   }
 }
